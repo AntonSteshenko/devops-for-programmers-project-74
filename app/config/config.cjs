b9441d39318060825e2bf10ba225477d8ef7e653
Anton Steshenko <risorsedaffari@gmail.com>
2023-09-04 18:43:43 +0200

    step 6 added

--- app/config/config.cjs
+++ app/config/config.cjs
@@ -1,12 +1,12 @@
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
   development: {
-    dialect: 'sqlite',
-    storage: './database.sqlite',
+    dialect: "sqlite",
+    storage: "./database.sqlite",
   },
   production: {
-    dialect: 'postgres',
+    dialect: "postgres",
     database: process.env.DATABASE_NAME,
     username: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD,
@@ -19,16 +19,16 @@ module.exports = {
       },
     },
   },
-  test: {
-    dialect: 'sqlite',
-    storage: './database.test.sqlite',
-  },
   // test: {
-  //   dialect: 'postgres',
-  //   database: process.env.DATABASE_NAME,
-  //   username: process.env.DATABASE_USERNAME,
-  //   password: process.env.DATABASE_PASSWORD,
-  //   port: process.env.DATABASE_PORT,
-  //   host: process.env.DATABASE_HOST,
+  //   dialect: 'sqlite',
+  //   storage: './database.test.sqlite',
   // },
+  test: {
+    dialect: "postgres",
+    database: process.env.DATABASE_NAME,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    port: process.env.DATABASE_PORT,
+    host: process.env.DATABASE_HOST,
+  },
 };
